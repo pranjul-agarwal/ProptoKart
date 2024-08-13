6eb2f2021291b44810ff3336c4f3eb8151c6206b
itstanvii <tanvijain1511@gmail.com>
2024-08-13 11:30:27 +0530

    Sign-up Functionality implemented

--- client/src/components/Sign/Signup.jsx
+++ client/src/components/Sign/Signup.jsx
@@ -1,18 +1,59 @@
 import React from 'react'
 import '../Sign/signup.css'
-import img from '../../assets/signup.jpg'
-import { Link } from 'react-router-dom'
+
+import { Link,useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+
 export default function Signup() {
+  const [formData,setFormData]=useState({});
+  const [error,setError]=useState(null);
+  const [loading,setLoading]=useState(false);
+  const navigate=useNavigate();
+  const handleChange=(e)=>{
+    setFormData({
+      ...formData,
+      [e.target.id]:e.target.value,
+        });
+  };
+  console.log(formData)
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    try{
+      setLoading(true);
+    const res= await fetch('/api/auth/signup',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(formData),
+    });
+   const data=await res.json();
+   if(data.success==false){
+      setLoading(false);
+      setError(data.message);
+      return;
+   }
+   setLoading(false);
+   setError(null);
+   navigate('/sign-in');
+    }
+    catch(error){
+      setLoading(false);
+      setError(error.message);
+  }
+};
   return (
     <div className="parent-container">
      <> 
   <div className='section'>
         <h1>Sign Up</h1>
-        <form className='signform'>
-            <input type="text" placeholder='Username' />
-            <input type="email" placeholder='Email' />
-            <input type="password" placeholder='Password' />
-            <button type='submit' >Sign Up</button>
+        <form className='signform' onSubmit={handleSubmit}>
+            <input type="text" placeholder='Username' id='username' onChange={handleChange} />
+            <input type="email" placeholder='Email' id='email' onChange={handleChange} />
+            <input type="password" placeholder='Password' id='password' onChange={handleChange}  />
+            <button disabled={loading} type='submit' >
+              {loading ? 'Loading...':'Sign Up'}
+            </button>
         </form>
         <div className='flex gap-2 mt-2'>
             <p>Have an account?</p>
@@ -20,6 +61,7 @@ export default function Signup() {
             <span className='text-red-600'>Sign in</span>
             </Link>
         </div>
+        {error && <p className='text-red-600'>{error}</p>}
    </div>
     </>
     </div>
