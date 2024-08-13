3c9e859b40e2731396937fe0d8069d1b1d7fffc6
Pranjul Agarwal <agarwalpranjul353@gmail.com>
2024-08-13 16:39:13 +0530

    created signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,22 +1,38 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
 
 export const signup = async (req, res, next) => {
 
     const { username, email, password } = req.body;
-    console.log(req.body)
 
     const hashedPassword = await bcryptjs.hashSync(password, 10);
-
 
     try {
         const newUser = new User({
             username, email, password: hashedPassword
         });
-        console.log(newUser)
+        console.log(newUser);
         await newUser.save();
         res.status(201).json('User created successfully');
 
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'user not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, "Invalid password!"));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(validUser);
     } catch (error) {
         next(error);
     }
