1a1a27d2b5af01558b01c8abc3e360af1f4c7490
itstanvii <tanvijain1511@gmail.com>
2024-08-14 13:08:39 +0530

    Added Redux Persist
    To store the user data inside the local storage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import {store} from './redux/store.js'
 import {Provider} from 'react-redux'
+import {persistor} from './redux/store.js'
+import {PersistGate} from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
   </Provider>,
 )
